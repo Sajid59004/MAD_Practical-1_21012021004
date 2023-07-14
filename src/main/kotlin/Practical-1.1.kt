dca4e5acac0eaf34a54f752b87e2f02d8d50501b
student <student@gnu.ac.in>
2023-07-14 08:28:54 +0530

    Created Practical-1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,25 @@
+import sun.font.TrueTypeFont
+
+fun main(){
+    var a: Int=5
+    var b: Float = 1.0F
+    var c: Char = 'V'
+    var d: String = "Doctor"
+    var e: Boolean = true
+    var f: Double = 1.22
+    var g: Long = 120151
+    var h: Short = -2
+    var i: Byte = 127
+
+    println("Integer value : $a ")
+    println("Float value : $b")
+    println("Character value : $c")
+    println("String value : $d")
+    println("Boolean value : $e")
+    println("Double value : $f")
+    println("Long value : $g")
+    println("Short value : $h")
+    println("Byte value : $i")
+
+
+}
