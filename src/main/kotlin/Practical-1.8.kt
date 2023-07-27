0b898c485b0a2be10911fd28d5f38fe94d3f9315
Sajid59004 <97504754+Sajid59004@users.noreply.github.com>
2023-07-27 15:36:26 +0530

    Update Practical-1.8.kt

--- src/main/kotlin/Practical-1.8.kt
+++ src/main/kotlin/Practical-1.8.kt
@@ -1,28 +1,28 @@
 fun main(){
 
     println("Array created by using arrayof() method")
-    var a = arrayOf(23,43,5,63,8)
-    println(a.contentToString())
+    var a1 = arrayOf(23,43,5,63,8)
+    println(a1.contentToString())
 
     println("Array created by using Array<>  method")
-    var b = Array(5){0}
-    println(b.contentDeepToString())
+    var a2 = Array(5){0}
+    println(a2.contentDeepToString())
 
     println("Array created by using Array<> and Lambda function")
-    var c = Array(7){i->i}
-    println(c.contentToString())
+    var a3 = Array(7){i->i}
+    println(a3.contentToString())
 
     println("Array created using IntArray()")
-    var d = IntArray(5){7}
-    println(d.joinToString (", "))
+    var a4 = IntArray(5){7}
+    println(a4.joinToString (", "))
 
     println("Array created using IntArrayOf()")
-    var e = intArrayOf(12,54,64,67,2)
-    println(e.joinToString(", "))
+    var a5 = intArrayOf(12,54,64,67,2)
+    println(a5.joinToString(", "))
 
     println("Array created by using arrayof() and intArrayOf()")
-    var f = arrayOf(intArrayOf(45,3), intArrayOf(4,1), intArrayOf(9,2))
-    println(f.contentDeepToString())
+    var a6 = arrayOf(intArrayOf(45,3), intArrayOf(4,1), intArrayOf(9,2))
+    println(a6.contentDeepToString())
 
     print("Enter number of Elements : ")
     val size : Int = readLine()!!.toInt()
@@ -37,4 +37,26 @@ fun main(){
     print("Entered Array = ")
     println(user.contentToString())
 
+    println("========================== With In-Built Function ==========================")
+    println("Array Sorted by in-built Function =  ")
+    user.sort()
+    println(user.contentToString())
+
+    val a7 = intArrayOf(45,96,689,593,-45,-76,-453,-56)
+    println("========================== Without In-Built Function ==========================")
+    println("Array Sorted without in-built Function = ")
+    println(user.contentToString())
+
+    var temp: Int
+    for (i in a7.indices) {
+        for (j in a7.indices) {
+            if (a7[j] > a7[i]) {
+                temp = a7[j]
+                a7[j] = a7[i]
+                a7[i] = temp
+            }
+        }
+    }
+    println("Array Sorted without in-built Function = ")
+    println(a7.contentToString())
 }
